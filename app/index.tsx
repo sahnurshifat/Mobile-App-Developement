b1214534c5d0f1a0b686d79f445d82ef92a3b667
sahnurshifat <Sahnurshifat9@gmail.com>
2026-04-27 15:24:27 +0600

    Second commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,13 @@ import { View, Text} from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, backgroundColor: "crimson" , padding: 50}}>
+    <View style={{ flex: 1, backgroundColor: "black" , padding: 50}}>
 
       <View style={{ width: 200, height: 200, backgroundColor: "yellow", padding: 50}} ></View>
 
       <View style={{ width: 200, height: 200, backgroundColor: "green", padding: 50}} ></View>
       <Text>
-        <Text style={{color:"Black"}}>Shifat</Text>
+        <Text style={{color:"white"}}>Shifat</Text>
       </Text>
     </View>
   );
