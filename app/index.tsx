fd7ab71d2a348ab3b17b21724a0ae97852023b58
sahnurshifat <Sahnurshifat9@gmail.com>
2026-04-27 15:44:57 +0600

    scrollview added

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text, Image} from "react-native";
+import { View, Text, Image, ScrollView, ImageBackground} from "react-native";
 
 const logoImg = require("@/assets/images/emoji4.png")
+const bgImg = require("@/assets/images/emoji1.png")
 
 export default function Index() {
   return (
+    <ScrollView style= {{flex:1}}>
     <View style={{ flex: 1, backgroundColor: "black" , padding: 50}}>
+      <ImageBackground source={bgImg} style = {{flex: 1}}/>
 
       <View style={{ width: 200, height: 200, backgroundColor: "yellow", padding: 50}} ></View>
 
@@ -13,7 +16,9 @@ export default function Index() {
         <Text style={{color:"white"}}>Shifat</Text>
       </Text>
       <Image source={logoImg} style={{width: 200, height: 200}}/>
+       <Image source={{uri:"https://picsum.photos/200"}} style={{width: 200, height: 200}}/>
     </View>
+</ScrollView>
   );
 }
  
